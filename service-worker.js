65e2529894cb1e03eba4a1c9c028f8d364e11112
RellzSilence <rellzstore001@gmail.com>
2025-08-03 07:47:01 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("elangbug-v1").then(cache => {
+      return cache.addAll(["index.html", "dashboard.html", "style.css", "script.js"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
+});
